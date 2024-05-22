7bd09d3c83b5bfe4a4d66cc31699c35c75c89e7b
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-05-22 19:16:54 +0300

    some upd

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,23 @@
+
+const API_KEY = '24251933-9d29857377778e751e7f0d7a1';
+const BASE_URL = 'https://pixabay.com/api/';
+
+    function fetchImage(q){
+        const params =  new URLSearchParams({
+        key: API_KEY,
+        q: searchWord,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+        })
+
+        return fetch(`${BASE_URL}?&${params}`).then(response => {
+            if(!response.ok) {
+                throw new Error(response.status)
+            }
+
+            return response.json();
+        })
+    };
+
